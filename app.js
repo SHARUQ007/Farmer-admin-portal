0488f0ade124e71316c44f4d764e1e08e085266b
programmerraja <boooathis123@gmail.com>
2021-07-02 15:08:24 +0530

    COnfigured for heroku

--- app.js
+++ app.js
@@ -57,14 +57,10 @@ app.use('/api/orders', ordersRouter);
 
 
 
-// Serve static assets if in production
-if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('client/build'));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
+// Send every other request to the React app
+// Define any API routes before this runs
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./frontend/build/index.html"));
+});
 
 module.exports  =app;
