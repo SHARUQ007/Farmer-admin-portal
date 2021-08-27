57a399042d47de50f722a593a875102c2f0558be
programmerraja <boooathis123@gmail.com>
2021-08-27 15:14:51 +0530

    Register page removed

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,7 +3,10 @@ const { Router } = require( "express");
 const authController = require( '../controllers/auth.controller.js');
 const authRouter = Router();
 
+const auth =require('../middleware/auth.js');
+const ROLES="SUPER_ROLES";
+
 authRouter.post('/login', authController.login);
-authRouter.post('/register', authController.register);
+// authRouter.post('/register',auth.isAuthenticatedAdmin,auth.hasPermission(ROLES),authController.register);
 
 module.exports  =  authRouter;
